5ef1ea02b09027f4d0e0b7d0b1bc41066b85d8d4
stong <498327580@qq.com>
2023-09-04 10:50:26 +0800

    init

--- gitxxx.py
+++ gitxxx.py
@@ -1,18 +1,33 @@
-
 from git import Repo
+from unidiff import PatchSet
 
 def git_diff():
-    # 初始化 Repo 对象
-    repo = Repo('.')
+    # 创建一个代表当前仓库的Repo对象
+    repo = Repo()
 
-    # 获取最后一次提交
-    last_commit = repo.head.commit
+    # 获取git diff的输出
+    diffs = repo.git.diff()
 
-    # 获取这次提交和上一次提交之间的差异
-    diffs = last_commit.diff('HEAD~1')
+    # 解析输出
+    patch_set = PatchSet(diffs)
 
+    files = []
     # 遍历每个差异
-    for diff in diffs.iter_change_type('M'):
-        print(f'Changed: {diff.a_path}')
+    for patch in patch_set:
+        file = {'file': patch.path, 'added': '', 'removed': ''}
+        for hunk in patch:
+            for line in hunk:
+                if line.value.strip() == "" or line.value.startswith('#'):
+                    continue
+                if line.is_added:
+                    file['added'] += line.value
+                elif line.is_removed:
+                    file['removed'] += line.value
+        files.append(file)
+    return files
 
-git_diff()
+files = git_diff()
+for file in files:
+    print(f"File: {file['file']}")
+    print(f"Added:\n{file['added']}")
+    print(f"Removed:\n{file['removed']}")
--- main.py
+++ main.py
@@ -44,35 +44,3 @@ if __name__ == '__main__':
 
     print(completion.choices[0].message)
     print(completion)
-
-'''mine
-It seems like you have provided a Python script that uses the `unidiff` library to parse and analyze differences in a Git repository.
-The script consists of a function `git_diff()` that returns a list of dictionaries containing information about the modified files.
-\n\nTo summarize the functionality of the script:\n\n
-1. Import the necessary `unidiff` module: `from unidiff import PatchSet`.\n\n
-2. Create a `Repo` object representing the current repository: `repo = Repo()`.\n\n
-3. Get the `git diff` output as a string: `diffs = repo.git.diff()`.\n\n
-4. Parse the diff string into a `PatchSet` object: `patch_set = PatchSet(diffs)`.\n\n
-5. Iterate over each patch in the `PatchSet` object:\n\n 
-  - Create a dictionary `file` to store information about the file with keys 'file', 'added', and 'removed'.\n\n
-  - Iterate over each hunk in the patch:\n\n     
-  - Iterate over each line in the hunk:\n\n       
-  - Skip empty lines and lines starting with '#' characters.\n\n   
-  - If the line is added, append its value to the 'added' field in the `file` dictionary.\n\n 
-  - If the line is removed, append its value to the 'removed' field in the `file` dictionary.\n\n  
-  - Append the `file` dictionary to the `files` list.\n\n
-6. Return the `files` list.\n\n
-7. Outside the `git_diff()` function, iterate over each file dictionary in the `files` list and print the file path, added lines, and removed lines.\n\n
-However, it seems like there are some parts of the code that are commented out or have non-ASCII charactershhgfhjdh65为其2 x If you need further assistance, please provide more information or clarify your intentions.
-'''
-
-'''devchat
-Refactor git_diff function to parse git diff output
-
-- Replace Repo('.') with Repo() to represent the current repository.
-- Replace the method of getting diffs. Now it gets git diff output directly instead of getting the difference between the last commit and the previous one.
-- Use PatchSet from unidiff library to parse the git diff output.
-- Add logic to skip empty lines and lines starting with '#'.
-- Store the parsed information (file name, added content, removed content) in a list of dictionaries.
-- Print out the parsed information instead of just printing the changed file paths.
-'''
